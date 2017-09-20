b43b956af6cd887cdaa00d23fc21cf6c8b66e7c5
Harish <Harish@Hony-PC>
2017-09-20 23:10:15 +0530

    adding additional output

--- SalesTool/src/Salesdata.java
+++ SalesTool/src/Salesdata.java
@@ -5,6 +5,7 @@ public class Salesdata {
 		// TODO Auto-generated method stub
 		int data[]={10,20,30};
 		System.out.println("Display Method ");
+		System.out.println("test123");
 		System.out.println("*******Show Data********");
 		for(int a:data){
 			System.out.println("Value ="+a);
