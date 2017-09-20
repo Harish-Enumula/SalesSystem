446e94740bdd860ed1b98ece88d0963c6f39774e
Harish <Harish@Hony-PC>
2017-09-20 20:45:15 +0530

    initial checkout test

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+public static void main(String args[])
+{
+	Salesdata data =new Salesdata();
+	welcome();
+	data.display();
+	
+	}
+static void welcome(){
+	System.out.println("welcome to ");
+	
+}
+}
